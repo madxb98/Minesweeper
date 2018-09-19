c7a984c92fabd0644e0bac9407610719a4c11ffa
Madison Brown <madxb98@gmail.com>
2018-09-19 18:16:35 -0500

    Added the Tile object class and its initializer

--- minesweeper.py
+++ minesweeper.py
@@ -1,3 +1,16 @@
 #!/bin/python3
 
 #Here is where you'll define the Tile objects Madison
+
+class Tile(object):
+	isBomb = bool
+
+	#For initializing a Tile object
+	def __init__(self, isBomb):
+		self.isBomb = isBomb
+
+def main():
+
+
+
+
