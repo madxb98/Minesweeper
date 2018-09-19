a85a4f648eed65038e543f35b228490702a19baa
Madison Brown <madxb98@gmail.com>
2018-09-19 18:56:37 -0500

    Fixing a recovery issue

--- minesweeper.py
+++ minesweeper.py
@@ -3,8 +3,8 @@
 #Here is where you'll define the Tile objects Madison
 
 class Tile(object):
-    
-    #For initializing a Tile object
+
+    #This is the initializer for the Tile class
     def __init__(self, is_bomb):
         self.is_bomb = is_bomb
 
