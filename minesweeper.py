e78236970157f7ead95137adaf32c7334be2ec3f
Madison Brown <madxb98@gmail.com>
2018-09-19 20:02:50 -0500

    Added a method that creates and returns a 2d array

--- minesweeper.py
+++ minesweeper.py
@@ -7,7 +7,20 @@ class Tile(object):
     def __init__(self, is_bomb):
         self.is_bomb = is_bomb
 
+#Creates and returns a 2d list
+def get_2d_list(width, height):
+    list = []
+    for i in range(width):
+        for j in range(height):	   
+            list.append(Tile(True))
+    return list
+
 def main():
+    nl = get_2d_list(3,3)
+    for i in range(len(nl)):
+        print(nl[i].is_bomb)
+
+main()
 
 
 
