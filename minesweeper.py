68e3e45f32136156f975235eca6ac9277eed45e7
Madison Brown <madxb98@gmail.com>
2018-09-22 01:52:27 -0500

    Logic for creating list is now list comprehension

--- minesweeper.py
+++ minesweeper.py
@@ -1,20 +1,15 @@
 #!/bin/python3
 
-#Here is where you'll define the Tile objects Madison
+#Creates and returns a 2d list
+def get_2d_list(width, height):
+    list = [[Tile(False) for _ in range(width)] for _ in range(height)]
+    return list
+
 class Tile(object):
 
     #This is the initializer for the Tile class
     def __init__(self, is_bomb):
         self.is_bomb = is_bomb
-
-#Creates and returns a 2d list
-def get_2d_list(width, height):
-    list = []
-    for i in range(width):
-        list.append([])
-        for j in range(height):	   
-            list[i].append(Tile(True))
-    return list
 
 def main():
     nl = get_2d_list(3,3)
